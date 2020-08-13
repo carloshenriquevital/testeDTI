b8e16353125bb3a9d158691676800eeddce71d60
Carlos Henrique dos Santos Vital <carloshenrique.vital@gmail.com>
2020-08-13 02:05:13 -0300

    Update ProductRepositoryTest.php

--- back/tests/Feature/ProductRepositoryTest.php
+++ back/tests/Feature/ProductRepositoryTest.php
@@ -25,6 +25,9 @@ class ProductRepositoryTest extends TestCase
         ]);
 
         $this->assertCount(1, Product::all());
+        $this->assertEquals('Produto 1', Product::first()->name);
+        $this->assertEquals(2, Product::first()->number);
+        $this->assertEquals(34.6, Product::first()->price);
     }
 
 
@@ -54,5 +57,49 @@ class ProductRepositoryTest extends TestCase
         $this->assertEquals('Produto Atualizado', Product::first()->name);
         $this->assertEquals(2, Product::first()->number);
         $this->assertEquals(34.6, Product::first()->price);
+    }
+
+    /** @test */
+    public function a_product_can_be_deleted()
+    {
+        $this->withExceptionHandling();
+        $productRepository = new EloquentProductRepository(new Product);
+
+        $productRepository->create([
+            'name' => 'Produto Novo',
+            'number' => 2,
+            'price' => 34.6
+        ]);
+
+        $this->assertCount(1, Product::all());
+
+        $this->assertEquals('Produto Novo', Product::first()->name);
+        $this->assertEquals(2, Product::first()->number);
+        $this->assertEquals(34.6, Product::first()->price);
+
+        $productRepository->delete(1);
+
+        $this->assertCount(0, Product::all());
+    }
+
+    /** @test */
+    public function a_product_can_be_retrieved()
+    {
+        $this->withExceptionHandling();
+        $productRepository = new EloquentProductRepository(new Product);
+
+        $productRepository->create([
+            'name' => 'Produto Novo',
+            'number' => 2,
+            'price' => 34.6
+        ]);
+
+        $this->assertCount(1, Product::all());
+        
+        $produto = $productRepository->findById(1);
+
+        $this->assertEquals('Produto Novo', $produto->name);
+        $this->assertEquals(2, $produto->number);
+        $this->assertEquals(34.6, $produto->price);
     }
 }
